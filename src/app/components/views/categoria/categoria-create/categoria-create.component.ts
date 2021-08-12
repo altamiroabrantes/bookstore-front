fc3e83e686131106e3098748417e4883eaf5aa4b
Altamiro <altamiro.abrantes@gmail>
2021-08-11 21:12:09 -0300

    Criaçao do componente CATEGORIA-CREATE Parte3

--- src/app/components/views/categoria/categoria-create/categoria-create.component.ts
+++ src/app/components/views/categoria/categoria-create/categoria-create.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { CategoriaService } from './../categoria.service';
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from '../categoria.model';
 
 @Component({
   selector: 'app-categoria-create',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaCreateComponent implements OnInit {
 
-  constructor() { }
+  categoria: Categoria = {
+    nome: '',
+    descricao: ''
+  }
+
+  constructor(private service: CategoriaService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  create(): void {
+    this.service.create(this.categoria).subscribe((resposta) => {
+      this.router.navigate(['categorias']);
+      this.service.mensagem('Categoria criada com sucesso!');
+    })
   }
 
 }
